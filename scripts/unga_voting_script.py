cb2f3d76f0732a19aba628d9beb87c488df09993
shannen <e0967956@u.nus.edu>
2025-03-21 02:00:32 +0800

    git add unga_voting_script.py git commit -m "Geopolitical Relations Script" git push origin main git push --set-upstream origin main

---
+++ scripts/unga_voting_script.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+agree_sc = pd.read_csv("../data/AgreementScoresAll_Jun2024.csv")
+cow = pd.read_csv("../data/COW-country-codes.csv")
+
+agree_sc = agree_sc[["ccode1", "ccode2", "agree", "year", "IdealPointAll.x", "IdealPointAll.y", "IdealPointDistance"]]
+
+cow_dict = {row['CCode']: row['StateNme'] for index, row in cow.iterrows()}
+
+#left join
+result = []
+
+for _, row in agree_sc.iterrows():
+    ccode1 = row['ccode1']
+    country1 = cow_dict.get(ccode1, None)
+    ccode2 = row['ccode2']
+    country2 = cow_dict.get(ccode2, None)
+    result.append(list(row) + [country1, country2])
+
+columns = agree_sc.columns.tolist() + ['Country1', 'Country2']
+df = pd.DataFrame(result, columns=columns)
+
+#filter target countries
+target_countries = ["China", "Hongkong", "Japan", "Korea", "Malaysia", "Saudi Arabia", "Thailand", "USA", "Singapore"]
+df = df[(df["Country1"].isin(target_countries)) & (df["Country2"].isin(target_countries))]
+
+#print("Unique Years in Filtered Data:", df["year"].unique())
+#1946 - 2023
+
+#impute IdealPointDistance values
+df.loc[df["IdealPointDistance"].isna(), "IdealPointDistance"] = (
+        df["IdealPointAll.x"] - df["IdealPointAll.y"]
+).abs()
+
+#print(df[df.duplicated()])
+#0
+
+#check for na values
+#print(df.isna().sum())
+#ccode1                  0
+#ccode2                  0
+#agree                   0
+#year                    0
+#IdealPointAll.x         6
+#IdealPointAll.y         6
+#IdealPointDistance      6
+#Country1                0
+#Country2                0
+
+#remove NA
+df = df.dropna()
+#total entries
+print(len(df))
+#1818
