9ed45cc3a51eefae4d05f4807ec006b733198f42
Zhang Jun Lu <zhangjunlu01@gmail.com>
2025-03-28 01:44:51 +0800

    fta script, with tariff weights in cleaned adjusted fta data

---
+++ scripts/FTA.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+# Read files
+fta_data = pd.read_csv("data/Free Trade Agreement Dataset.csv")
+tariff_weights_2016 = pd.read_csv("data/tariff_weights_2016.csv")
+
+fta_data = pd.merge(fta_data, tariff_weights_2016[['Country', 'tariff_weight']], 
+                    left_on='Country', right_on='Country', how='left')
+
+
+fta_data.rename(columns={'tariff_weight': 'tariff_weight_2016_Country'}, inplace=True)
+fta_data = pd.merge(fta_data, tariff_weights_2016[['Country', 'tariff_weight']], 
+                    left_on='Partner Country', right_on='Country', how='left')
+
+fta_data.rename(columns={'tariff_weight': 'tariff_weight_2016_Partner'}, inplace=True)
+
+fta_data = fta_data.fillna(0)
+
+#formula
+fta_data['Adjusted_value'] = np.where(
+    fta_data['Year'] < 2017,  
+    (fta_data['FTA Weight'] +fta_data['Other Agreements'] - 
+     (fta_data['Tariff %']/100 * fta_data['Weight in Exports']/100 * fta_data['tariff_weight_2016_Country']) -
+     (fta_data['Tariff %']/100 * fta_data['Weight in Exports']/100 * fta_data['tariff_weight_2016_Partner'])),  # If True (Year < 2017)
+    fta_data['FTA Weight'] + fta_data['Other Agreements'] - (fta_data['Tariff %']/100 * fta_data['Weight in Exports']/100)  # If False (Year >= 2017), no tariff weight applied
+)
+
+#Cleaning
+fta_data.rename(columns={'Country_x': 'Country'}, inplace=True)
+
+# Select only the relevant columns: Country, Partner Country, Adjusted_value
+final_data = fta_data[['Year', 'Country', 'Partner Country', 'Adjusted_value']]
+
+final_data.to_csv("data/adjusted_fta_data.csv", index=False)
